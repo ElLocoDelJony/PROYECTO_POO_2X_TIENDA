3014cd7efd0603ce9dbb1b4018bcfcfc4da2c9d2
OfficialMYKE <michaelcarrillo.ec@gmail.com>
2026-01-03 20:02:48 -0500

    first commit

---
+++ src/View/Login.java
@@ -0,0 +1,13 @@
+package View;
+
+import javax.swing.*;
+
+public class Login extends JFrame{
+    private JTextField txtUsuario;
+    private JPasswordField txtPassword;
+    private JButton btnIngresar;
+    private JPanel pnlLogin;
+    private JPanel pnlBanner;
+    private JPanel pnlConetnt;
+
+}
---
+++ src/View/Menu.java
@@ -0,0 +1,24 @@
+package View;
+
+import javax.swing.*;
+
+public class Menu {
+    private JPanel pnlMenu;
+    private JPanel pnlSidebar;
+    private JButton btnHome;
+    private JButton btnSolicitantes;
+    private JButton btnExamenes;
+    private JButton btnTramites;
+    private JButton btnUsuarios;
+    private JButton btnSalir;
+    private JPanel pnlHeader;
+    private JLabel lblBreadcrumb;
+    private JLabel lblUser;
+    private JPanel pnlContent;
+    private JLabel lblWelcome;
+    private JPanel pnlStatsContainer;
+    private JLabel lblStat1;
+    private JLabel lblStat2;
+    private JLabel lblStat3;
+    private JLabel lblStat4;
+}
