f06489d8c24351e54f24d78e71b3780d53ca886d
OfficialMYKE <michaelcarrillo.ec@gmail.com>
2026-01-03 21:03:48 -0500

    Pantallas, Solicitantes y VerificacionRequisitos

---
+++ src/View/Solicitantes.java
@@ -0,0 +1,18 @@
+package View;
+
+import javax.swing.*;
+
+public class Solicitantes {
+    private JTextField txtCedula;
+    private JTextField txtNombres;
+    private JTextField txtApellidos;
+    private JTextField txtDireccion;
+    private JTextField txtEmail;
+    private JTextField txtTelefono;
+    private JComboBox cmbTipo;
+    private JComboBox cmbSangre;
+    private JButton btnLimpiar;
+    private JButton btnGuardar;
+    private JButton btnRegresar;
+    private JPanel pnlSolicitante;
+}
---
+++ src/View/VerificacionRequisitos.java
@@ -0,0 +1,19 @@
+package View;
+
+import javax.swing.*;
+
+public class VerificacionRequisitos {
+    private JButton btnRegresar;
+    private JTextField txtBuscarCedula;
+    private JButton btnBuscar;
+    private JLabel lblNombreCompleto;
+    private JCheckBox chkCedula;
+    private JCheckBox chkPapeleta;
+    private JCheckBox chkPsicosensometrico;
+    private JCheckBox chkTeorico;
+    private JCheckBox chkPago;
+    private JTextArea txtObservaciones;
+    private JButton btnRechazar;
+    private JButton btnAprobar;
+    private JPanel pnlVerificacion;
+}
